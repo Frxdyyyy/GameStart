9a59caf91041363425f8550c00dfe671f24f181a
Cristobal <oyarzuncristobal353@gmail.com>
2025-05-19 20:12:26 -0400

    Update Juego.java

---
+++ src/main/java/com/GameStart/GameStart/Model/Juego.java
@@ -0,0 +1,12 @@
+package com.GameStart.GameStart.Controller;
+
+public class Juego {
+
+    def __init__(self, id, titulo, precio, descripcion):
+        self.id = id
+        self.titulo = titulo
+        self.precio = precio
+        self.descripcion = descripcion
+
+}
+
